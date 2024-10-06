82f4d339baf99d5c0b909c9ca6b300021070f5e4
titusefferian <kiritosato@gmail.com>
2024-10-06 10:13:39 +0900

    add secret manager

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,6 +2,7 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import * as kms from 'aws-cdk-lib/aws-kms'
+import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
@@ -15,3 +16,12 @@ const customResourcesStack = backend.createStack('MyCustomResourcesForKMS')
 
 const helloWorldKey = new kms.Key(customResourcesStack,'hello world')
 helloWorldKey.addAlias('alias/foo')
+
+// Templated secret with username and password fields
+new secretsmanager.Secret(customResourcesStack, 'TemplatedSecret', {
+  generateSecretString: {
+    secretStringTemplate: JSON.stringify({ username: 'postgres' }),
+    generateStringKey: 'password',
+    excludeCharacters: '/@"',
+  },
+});
